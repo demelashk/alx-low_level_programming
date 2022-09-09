9337932682ec2b4de1f9dddeaac4fe3d51fd4aab
demelashk <demelash1452@gmail.com>
2022-09-09 06:02:33 -0700

    commit five

---
+++ 0x18-dynamic_libraries/nramdom.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * printf - writes out the out put.
+ * weite - writes out the string.
+ * exit - exit status.
+ */
+
+int printf(const char *format, ...)
+{
+    write(1, "9 8 10 24 75 -9\n", 17);
+    write(1, "Congratulations, you win the Jackpot!\n", 38);
+    exit(EXIT_SUCCESS);
+}
+
